4ca0edf04114fb26bfb01be3f802b381bc5f9833
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2019-08-11 12:19:26 +0200

    Working version for volume control and external on off switch

---
+++ ScreenerButtonTest_v1/Engine.ino
@@ -0,0 +1,119 @@
+#define EngineVersion 3
+
+#define EngineStartFolder 1
+#define EngineLowFolder 2
+#define EngineUpFolder 3
+#define EngineHighFolder 4
+#define EngineDownFolder 5
+#define EngineStopFolder 6
+#define InitializedBeep 7
+
+#define WarningBeepTrackNumber 1
+
+void StartPlayingWarningBeep() {
+  if (!crusherIsMoving) {
+    Serial.println(F("StartPlayingIntercut(WarningBeepTrackNumber)"));
+    StartPlayingIntercut(WarningBeepTrackNumber);
+  }
+}
+
+void StopPlayingWarningBeep() {
+  if (crusherIsMoving) {
+    Serial.println(F("StopPlayingIntercut()"));
+    StopPlayingIntercut();
+  }
+}
+
+void PlayInitializedBeep() {
+  Serial.println(F("PlayFolderTrack(InitializedBeep, EngineVersion)"));
+  PlayFolderTrack(InitializedBeep, EngineVersion);
+}
+
+void StartEngine() {
+  if (!engineIsRunning) {
+    engineIsRunningHigh = false;
+    engineIsRunning = true;
+
+    //Play startup
+    Serial.println(F("PlayFolderTrack(EngineStartFolder, EngineVersion)"));
+    PlayFolderTrack(EngineStartFolder, EngineVersion);
+    //always wait 1 sec to allow the player to start playing
+
+    //Wait till ready / delay Xms
+    wait(1);//wait(500);
+
+    //Start loop engine low
+    Serial.println(F("RepeatPlaybackFolder(EngineLowFolder)"));
+    RepeatPlaybackFolder(EngineLowFolder);
+  }
+}
+
+void EngineUp() {
+  //Play rev up
+  Serial.println(F("PlayFolderTrack(EngineUpFolder, EngineVersion)"));
+  PlayFolderTrack(EngineUpFolder, EngineVersion);
+  //Wait till ready / delay Xms
+  wait(1);//wait(1500);
+
+  //Start loop engine high
+  Serial.println(F("RepeatPlaybackFolder(EngineHighFolder)"));
+  RepeatPlaybackFolder(EngineHighFolder);
+//  StartQuickRedBlink();
+  engineIsRunningHigh = true;
+}
+
+void EngineDown() {
+  //Play rev down
+  Serial.println(F("PlayFolderTrack(EngineDownFolder, EngineVersion)"));
+  PlayFolderTrack(EngineDownFolder, EngineVersion);
+
+  //Wait till ready / delay Xms
+  wait(1);//wait(2500);
+
+  //Start loop engine low
+  Serial.println(F("RepeatPlaybackFolder(EngineLowFolder)"));
+  RepeatPlaybackFolder(EngineLowFolder);
+//  StopQuickRedBlink();
+  engineIsRunningHigh = false;
+}
+
+void StopEngine() {
+  if (engineIsRunning) {
+    //check if engine is high
+    if (engineIsRunningHigh) {
+      EngineDown();
+    }
+    //Play engine stop
+    Serial.println(F("PlayFolderTrack(EngineStopFolder, EngineVersion)"));
+    PlayFolderTrack(EngineStopFolder, EngineVersion);
+    wait(1);//wait(1500);
+    engineIsRunning = false;
+  }
+}
+
+bool PlaySequence(int sequenceLength, int sequence[5]) {
+  for (int i = 0; i < sequenceLength; i++) {
+    //Play sequence item
+
+    //Play folder sequence[i];
+
+    //Wait till ready / delay Xms
+
+  }
+  return true;
+}
+
+bool PlaySequenceAndLoop(int sequenceLength, int sequence[5]) {
+  for (int i = 0; i < sequenceLength - 1; i++) {
+    //Play sequence item
+
+    //Play folder sequence[i];
+
+    //Wait till ready / delay Xms
+  }
+  //PlayLoop last item from sequence
+
+  return true;
+}
+
+
---
+++ ScreenerButtonTest_v1/ScreenerButtonTest_v1.ino
@@ -0,0 +1,57 @@
+#include <OSL_SimpleTimer.h>
+#include <JC_Button.h>
+#include "defines.h"
+#include "variables.h"
+#include <PinChangeInterrupt.h>
+
+
+void setup() {
+  startup = true;
+  
+  //initialize boot vars
+  Serial.begin(9600);
+  Serial.println(F("Setting up"));
+
+  //initialize audio player on serial comms
+  Serial2.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(PLAYER_BUSY_PIN), SetPlayerFree, RISING);
+
+  delay(2500);
+  volumeLevel = 10;
+}
+
+void loop() {
+  if (startup) {
+    Serial.println(F("Startup loop entering"));
+
+    delay(3000);
+    setVolume(volumeLevel);
+   
+    Serial.println(F("Startup loop completed"));
+    startup = false;
+    PlayInitializedBeep();
+    delay(1000);
+  //  StartEngine();
+//    delay(10000);
+//    EngineUp();
+//    delay(10000);
+//    EngineDown();
+//    delay(10000);
+//    StopEngine();
+  }
+
+  volumeUpButton.read();
+  volumeDownButton.read();
+  powerButton.read();
+  lightsButton.read();
+  trackLeftForwardButton.read();
+  trackLeftBackwardButton.read();
+  trackLeftForwardButton.read();
+  trackRightBackwardButton.read();
+
+  HandleVolumeButtons();
+  HandleOnOffButton();
+
+  timer.run();
+}
